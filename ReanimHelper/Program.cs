4b3e6a47537155465ad290d77224c51c3589ae6c
mcxufly <mcxufly@outlook.com>
2025-05-01 04:35:34 +0800

    clear

--- ReanimHelper/Program.cs
+++ ReanimHelper/Program.cs
@@ -6,10 +6,5 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Reanim reanim = Utils.LoadFromCompiled(args[0]);
-		string file = Path.GetFileName(args[0]).Split('.')[0];
-		string reanimStr = JsonSerializer.Serialize<Reanim>(reanim);
-		using StreamWriter writer = new StreamWriter($"{file}.json");
-		writer.Write(reanimStr);
 	}
 }
